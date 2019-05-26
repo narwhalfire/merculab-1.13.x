70199139e53cdc47b1c45053108ed336a8cf4072
NarwhalFire <narwhalfire@scottnotfound.net>
2019-05-26 00:58:28 -0400

    models - beaker is better - flask is broke af

--- src/main/java/net/scottnotfound/merculab/block/labware/BlockBeaker.java
+++ src/main/java/net/scottnotfound/merculab/block/labware/BlockBeaker.java
@@ -3,6 +3,9 @@ package net.scottnotfound.merculab.block.labware;
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.world.IBlockReader;
 import net.minecraftforge.common.extensions.IForgeBlock;
 import net.scottnotfound.merculab.init.MercuLab;
@@ -10,6 +13,8 @@ import net.scottnotfound.merculab.init.MercuLab;
 import javax.annotation.Nullable;
 
 public class BlockBeaker extends Block implements IForgeBlock {
+    protected static final VoxelShape SHAPE = Block.makeCuboidShape(4.0, 0.0, 4.0,
+                                                                    12.0, 10.0, 12.0);
 
     public BlockBeaker(Properties properties) {
         super(properties);
@@ -24,6 +29,21 @@ public class BlockBeaker extends Block implements IForgeBlock {
     @Override
     public TileEntity createTileEntity(IBlockState state, IBlockReader world) {
         return MercuLab.TileEntityTypes.BEAKER.create();
+    }
+
+    @Override
+    public BlockRenderLayer getRenderLayer() {
+        return BlockRenderLayer.CUTOUT;
+    }
+
+    @Override
+    public boolean isFullCube(IBlockState state) {
+        return false;
+    }
+
+    @Override
+    public VoxelShape getShape(IBlockState state, IBlockReader worldIn, BlockPos pos) {
+        return SHAPE;
     }
 
 }
